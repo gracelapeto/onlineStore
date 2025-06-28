0736313c421651999d6efa6547e67077b65b216a
Gracela Peto <petogracela@gmail.com>
2025-06-28 11:36:21 +0200

    Update

--- src/main/java/com/example/onlineStore/entities/User.java
+++ src/main/java/com/example/onlineStore/entities/User.java
@@ -2,11 +2,23 @@ package com.example.onlineStore.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import org.aspectj.weaver.ast.Var;
 
 @Entity
+@Data
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String city;
+    private String address;
+    private String avatar;
+
+
 
 }
