938448026e90d32649d60fe5b1e15d59af75fe22
Arvenola <dorearvenola@gmail.com>
2025-06-28 15:50:54 +0200

    CreateAuthor CreateAuthorRepository CreateAuthorService CreateAuthorServiceImpl

--- src/main/java/com/example/onlineStore/repositories/AuthorRepository.java
+++ src/main/java/com/example/onlineStore/repositories/AuthorRepository.java
@@ -3,5 +3,10 @@ package com.example.onlineStore.repositories;
 import com.example.onlineStore.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long>{
+    Optional<Object> findByFirstName(String firstname);
+
+    Optional<Object> findByName(String name);
 }
