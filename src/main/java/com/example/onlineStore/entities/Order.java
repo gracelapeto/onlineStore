1bdaa34e7b99c8b5ed6bdfed75e90e3d79248038
Klaudia Lazaj <klaudialazaj1@gmail.com>
2025-06-28 10:32:03 +0200

    update entities

---
+++ src/main/java/com/example/onlineStore/entities/Order.java
@@ -0,0 +1,24 @@
+package com.example.onlineStore.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private Long price;
+    private LocalDateTime orderDate;
+
+
+
+
+
+}
