ac83e1b999d8b1abe56e4e76bdb3a9854d18986b
Gracela Peto <petogracela@gmail.com>
2025-06-29 10:34:36 +0200

    Update

--- src/main/java/com/example/onlineStore/repositories/CategoryRepository.java
+++ src/main/java/com/example/onlineStore/repositories/CategoryRepository.java
@@ -3,8 +3,9 @@ package com.example.onlineStore.repositories;
 import com.example.onlineStore.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-
+    Optional<Category> findByNameIgnoreCase(String name);
 
 }
