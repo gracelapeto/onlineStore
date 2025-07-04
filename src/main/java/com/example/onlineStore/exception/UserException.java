6c10c67904bb0a978d41aa5415ad5418bfaaee7b
Gracela Peto <petogracela@gmail.com>
2025-07-04 21:29:29 +0200

    update

--- src/main/java/com/example/onlineStore/exception/UserException.java
+++
@@ -1,9 +0,0 @@
-package com.example.onlineStore.exception;
-
-public class UserException extends RuntimeException {
-
-    public static UserException userAlreadyExistsException(String username) {
-        String message = String.format("User with username %s exists!", username);
-        return new UserException(message);
-    }
-}
