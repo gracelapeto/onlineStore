dc7d97858c0f266fefd74ccced9e3ca3db135a3d
Gracela Peto <petogracela@gmail.com>
2025-07-04 21:35:34 +0200

    update

--- src/main/java/com/example/onlineStore/repositories/OrderLineRepository.java
+++ src/main/java/com/example/onlineStore/repositories/OrderLineRepository.java
@@ -3,5 +3,8 @@ package com.example.onlineStore.repositories;
 import com.example.onlineStore.entities.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
+    List<OrderLine> findByOrderId (Long orderId);
 }
